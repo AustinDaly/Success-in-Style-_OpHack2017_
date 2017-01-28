9c6cc40eaa2470e0c19ad6d357b7225c130b1a67
Mister 4 Eyes <austindaly7cf@gmail.com>
2017-01-28 11:17:45 -0500

    Changed index to follow express format.

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-const http  = require('http');
-const fs    = require('fs');
-const hostname = '192.168.1.8';
 const port = 3000;
 
-var files =
-[
-  "./Test.html"
-];
+var express = require("express");
+var app = express();
 
-var server = http.createServer( (req, res) =>
+app.get('/', function(reg, res)
 {
-  fs.readFile(files[0], "utf8", function(err, data)
-  {
-    res.writeHead(200, {
-            'Content-Type': 'text/html',
-                'Content-Length': data.length
-        });
-        res.write(data);
-        res.end();
-  });
+  res.send('Hello, world!');
 });
 
-server.listen(port, hostname, () =>
-{
-	console.log('Server started on port '+port);
+app.listen(port, function(){
+  console.log("Listening on port "+port);
 });
