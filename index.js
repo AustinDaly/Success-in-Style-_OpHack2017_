3b09fd70c8ff77c3fece16ddabca43b8e0194626
Mister 4 Eyes <austindaly7cf@gmail.com>
2017-01-28 10:45:30 -0500

    Added test code to send html files.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const http = require('http');
-
+const http  = require('http');
+const fs    = require('fs');
 const hostname = '192.168.1.8';
 const port = 3000;
 
-var server = http.createServer( (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('okay');
+var files =
+[
+  "./Test.html"
+];
+
+var server = http.createServer( (req, res) =>
+{
+  fs.readFile(files[0], "utf8", function(err, data)
+  {
+    res.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+  });
 });
 
 server.listen(port, hostname, () =>
