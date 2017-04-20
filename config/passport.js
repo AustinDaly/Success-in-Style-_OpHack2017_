316f675ce49c4242c738d9d0e7e568eb3eafe29f
Austin Daly <mister4eyez@gmail.com>
2017-04-20 11:13:24 -0400

    Setup MEAN stack once again.

---
+++ config/passport.js
@@ -0,0 +1,41 @@
+﻿var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy(
+	{
+		usernameField: 'email'
+	},
+	function (username, password, done)
+	{
+		User.findONe({ email: username }, 
+		function (err, user)
+		{
+			//Using specific messages instead of generic ones for debugging.
+			//Will change later
+			if (err)
+			{ return done(err); }
+			if (!user)
+			{
+				return done(null, false, 
+				{
+					message: 'Incorrect username.'
+				});
+			}
+			
+			if (!user.validPassword(password))
+			{
+				return done(null, false, 
+					{
+					message: 'Incorrect password.'
+				}
+				);
+			}
+			
+			//Passed all checks.
+			return done(null, user);
+		}
+		);
+	}
+));
