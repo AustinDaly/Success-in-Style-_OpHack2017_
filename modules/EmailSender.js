ad822a8b783c5e5f90c4ef46fb43c6667de0cf8d
Austin Daly <mister4eyez@gmail.com>
2017-04-26 15:04:26 -0400

    Added a default mail sender function to be used anywhere.

---
+++ modules/EmailSender.js
@@ -0,0 +1,35 @@
+'use strict';
+var nodemailer = require('nodemailer')
+
+module.exports =
+{
+  sendEmail: function(address, subject, message)
+  {
+    let transporter = nodemailer.createTransport(
+      {
+      service: 'gmail',
+      auth:
+      {
+        user: 'successinstylebot@gmail.com',
+        pass: '53aeeced9b16cba7cc049fedf104f4d969470d2c'
+      }
+    });
+
+    let mailOptions =
+    {
+      from: 'Success In Style <successinstylebot@gmail.com>',
+      to: address,
+      subject: subject,
+      text: message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) =>
+    {
+      if (error)
+      {
+        return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+    })
+  }
+};
